0c680faa14a06f447caab84f58a9bd23143f4708
Jaiveer Chadda <jaiveer.chadda@gmail.com>
2019-09-15 17:49:44 +0400

    Added the add_child function to Node class, and started making the input

--- tree.py
+++ tree.py
@@ -1,11 +1,20 @@
 
 class Node:
-    def __init__(self, value, operator=False, left=None, right=None):
+    def __init__(self, value, left_child=None, right_child=None, label=""):
         self.value = value
-        self.operator = operator
-        self.left = left
-        self.right = right
+        self.operator = True if type(value) == string else False
 
-rootNode = Node(5)
+        self.left_child = left_child
+        self.right_child = right_child
 
-print(rootNode)
+    def add_child(self, child_to_add, position):
+        if position in ["left", "l", "0", 0]:
+            self.left_child = child_to_add
+
+        elif position in ["right", "r", "1", 12]:
+            self.right_child = child_to_add
+
+
+root = Node(input("Enter a root node value"))
+
+while 1:

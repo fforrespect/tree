843878b1e3b896c044ed0bb6a166e6561182327d
Jaiveer Chadda <jaiveer.chadda@gmail.com>
2019-09-16 10:01:03 +0400

    Started making something but didnt rly work

--- tree.py
+++ tree.py
@@ -1,8 +1,23 @@
 
+nodes = {"/":"+", "0":"x", "00":"3", "01":"4", "1":"5"}
+
+def draw_tree(dict_of_values):
+    output = nodes["/"]
+
+
+    pass
+
+
+
+
+
+
+
+
 class Node:
     def __init__(self, value, left_child=None, right_child=None, label=""):
         self.value = value
-        self.operator = True if type(value) == string else False
+        self.operator = True if type(value) == "string" else False
 
         self.left_child = left_child
         self.right_child = right_child
@@ -11,10 +26,13 @@ class Node:
         if position in ["left", "l", "0", 0]:
             self.left_child = child_to_add
 
-        elif position in ["right", "r", "1", 12]:
+        elif position in ["right", "r", "1", 1]:
             self.right_child = child_to_add
 
 
-root = Node(input("Enter a root node value"))
+root = Node(input("Enter a root node value:\n>>> "))
+
+nodes = {"/": root}
 
 while 1:
+    what_to_do = input("\nAdd a child node?\nl - left\nr - right\n[nothing] - leave this as a leaf node\n>>> ")
